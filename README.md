9c447653bd5b8a2dfcd0afca2a83122632650804
Sam <sabd0044@student.monash.edu>
2023-12-12 00:09:17 +1100

    trying to fix the link for sidebar

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Money tracker to help me spend less
 STOP BEING DUMB WITH MONEY FR FR
 
 ### Features 
-top left total money in vs out 
+top left total money in vs out
 top right pie chart of categories
 bottom left list of transactions
 comments
--- tracker/src/components/Sidebar.js
+++ tracker/src/components/Sidebar.js
@@ -1,13 +1,17 @@
 import '../styles/Sidebar.css'
 import logo from '../assets/logo.png';
-import 
+import {BrowserRouter as Link} from 'react-router-dom';
 function Sidebar(){
 
     return (
         <div className='SidebarContainer'>
-            <div className='logo-img' >
-                <img src = {logo} className='img'></img>
-            </div>
+            
+                <div className='logo-img' >
+                    <Link to = '/'>
+                        <img src = {logo} alt = "Logo of broke human being" className='img'/>
+                    </Link>
+                </div>
+            
         </div>
     )
 }
